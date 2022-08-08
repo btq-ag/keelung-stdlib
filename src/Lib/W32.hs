b00b5a86948f457ce1758212016cc6f6c4ed5ae4
Thing-han, Lim <han.lim@chelpis.com>
2022-08-08 13:39:38 +0800

    [ new ] Add Lib.W32 module

---
+++ src/Lib/W32.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+
+module Lib.W32 where
+
+import Control.Monad
+import Data.Bits (Bits (testBit))
+import Data.Word (Word32)
+import Keelung
+import qualified Lib.Array as Arr
+import Lib.W8 (W8)
+
+type W32 = 'Arr 'Bool
+
+fromWord32 :: Word32 -> Comp n (Val W32 n)
+fromWord32 word = toArray $ map (Boolean . testBit word) [0 .. 31]
+
+zero :: Comp n (Val W32 n)
+zero = Arr.zeroBits 32
+
+zeros :: Int -> Comp n (Val ('Arr W32) n)
+zeros n = zero >>= Arr.replicate n
+
+fromW8 :: Val W8 n -> Comp n (Val W32 n)
+fromW8 = Arr.cast 32
+
+fromW8Chunks :: Val ('Arr W8) n -> Comp n (Val ('Arr W32) n)
+fromW8Chunks = Arr.flatten >=> Arr.chunks 32
