be25687549f77ab3dadaaa185de1220aa32bd22d
Thing-han, Lim <han.lim@chelpis.com>
2022-09-12 15:06:37 +0800

    [ new ][ test ] Add modules and top level tests

--- test/Test.hs
+++ test/Test.hs
@@ -1,9 +1,12 @@
 import Test.Tasty
 import qualified Test.Lib.Array as Array
+import qualified Test.Lib.W32 as W32
+import qualified Test.BLAKE2s as BLAKE2s
+import qualified Test.Util as Util
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
 tests = testGroup "Tests"
-    [Array.tests]
+    [Util.tests, Array.tests, W32.tests, BLAKE2s.tests]
