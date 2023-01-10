31bada0f86e9850b1423767498d4164ede3242c3
Viktor <vic@btq.li>
2023-01-10 17:32:06 +0800

    test workflows

--- gen.sh
+++ gen.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 stack exec -- profile $GITHUB_WORKSPACE
+find $GITHUB_WORKSPACE -name "*.prof" -exec ~/.local/bin/ghc-prof-flamegraph --alloc $f -o $GITHUB_WORKSPACE/results/$f-alloc.svg \;
+find $GITHUB_WORKSPACE -name "*.prof" -exec ~/.local/bin/ghc-prof-flamegraph --time $f -o $GITHUB_WORKSPACE/results/$f-time.svg \;
