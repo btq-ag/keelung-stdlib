6db875da3809dc7ee3c87fd3300b6e6b84524825
Viktor <vic@btq.li>
2023-02-06 21:37:02 +0800

    [ change ] ECC example

--- src/ECC.hs
+++ src/ECC.hs
@@ -1,9 +1,33 @@
 {-# LANGUAGE DataKinds #-}
 module ECC where
 import Keelung
+import Keelung.Monad
+import Keelung.Syntax.Typed (Expr(..))
+
+import Data.Maybe
+
 
 type EC = (Field, Field)
-type Point = (EC, Field, Field)
+newtype Point = Point (EC, Field, Field)
+  deriving (Eq, Show)
+
+instance Reusable Point where
+  reuse p@(Point ((a, b), x, y)) = do
+    a' <- reuse a
+    b' <- reuse b
+    x' <- reuse x
+    y' <- reuse y
+    return $ Point ((a', b'), x', y')
+
+-- instance Encode Point where
+--   encode ((a, b), x, y) = do
+--     xs <- mapM encode [a,b,x,y]
+--     let xs' = map (fromMaybe (error "") . decode) xs
+--     return $ Misc (xs', [], [])
+
+-- decode :: Array Int Expr -> Point
+-- decode arr = let [a,b,x,y] = toList arr
+--               in (a, b, x, y)
 
 run :: Int -> Comp (Field, Field)
 run n = do
@@ -12,27 +36,27 @@ run n = do
     x <- inputField
     y <- inputField
     assert ((y * y) `eq` ((x * x * x) + (x * a) + b))
-    let (_, x', y') = ((a, b), x, y) `times` n
+    Point (_, x', y') <- Point ((a, b), x, y) `times` n
     return (x', y')
   where
-    times :: Point -> Int -> Point
-    times point 1 = point
-    times point number = 
-        if even number then double (point `times` (number `div` 2))
-                       else (point `times` (pred number)) `add` point
+    times :: Point -> Int -> Comp Point
+    times point 1 = return point
+    times point number = do
+        if even number then (point `times` (number `div` 2)) >>= reuse >>= \p -> return $ double p
+                       else (point `times` (pred number)) >>= reuse >>= \p -> return $ p `add` point
     add :: Point -> Point -> Point
-    add p0@(ec, x0, y0) p1@(_, x1, y1)
+    add p0@(Point (ec, x0, y0)) p1@(Point (_, x1, y1))
       | p0 == p1 = double p0
-      | x0 == x1 = (ec, 0, 0)
-      | otherwise = (ec, x2, y2)
+      | x0 == x1 = Point (ec, 0, 0)
+      | otherwise = Point (ec, x2, y2)
       where
         slope = (y1 - y0) / (x1 - x0)
         x2 = slope * slope - (x0 + x1)
         y2 = (x0 - x2) * slope - y1
     double :: Point -> Point
-    double ((a, b), x, y)
-      | y == 0 = ((a, b), 0, 0)
-      | otherwise = ((a, b), x0, y0)
+    double (Point ((a, b), x, y))
+      | y == 0 = Point ((a, b), 0, 0)
+      | otherwise = Point ((a, b), x0, y0)
         where
           slope = (x * x * 3 + a) / (y + y)
           x0 = slope * slope - x - x
