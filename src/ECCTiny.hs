2c7f913e1bca03b39653d2684d71ddad8655b56a
Shang-Yi Yang <ilway25@gmail.com>
2023-04-06 14:41:01 +0800

    [ add ] var-scalarmult for tiny ECC

--- src/ECCTiny.hs
+++ src/ECCTiny.hs
@@ -10,10 +10,17 @@ import Keelung
 -- slopeDbl < (p*p*3+a)*p < 2^36
 type F = UInt 36
 
-_p, _a, _b :: F
-_p = 2833
+-- The programmer is responsible for ensuring that the following parameters constitute a valid ECDSA parameter.
+__p :: Integer
+__p = 2833
+
+_p, _a, _b, _n :: F
+_g :: (F, F)
+_p = UInt __p
 _a = 1
 _b = 1
+_g = (1341, 854) -- Generator
+_n = 131         -- Order of g
 
 -- | Performs eager modulo operation with respect to p.
 -- It is important to be mindful of potential overflows (exceeding the bit-width) and underflows (during subtraction),
@@ -21,9 +28,12 @@ _b = 1
 modP :: F -> Comp F
 modP n = snd <$> performDivMod n _p
 
+inverse :: F -> F
+inverse n = modInv n __p
+
 -- | Find a^-1 mod p with Fermat's Little Theorem. Use eager reduction.
-inverse :: F -> Comp F
-inverse num = foldM f 1 pm2_bitsrev
+inverseFermat :: F -> Comp F
+inverseFermat num = foldM f 1 pm2_bitsrev
   where
     pm2_bitsrev = reverse $ map ((_p - 2) !!!) [0 .. widthOf _p - 1]
     f n bit = do
@@ -58,8 +68,8 @@ smult' n = do
   smult n (Point (x, y))
 
 smult :: Int -> Point -> Comp (F, F)
-smult n pp@(Point (x, y)) = do
-  assert =<< onCurve pp
+smult n p@(Point (x, y)) = do
+  assert =<< onCurve p
   Point (x', y') <- Point (x, y) `times` n
   return (x', y')
   where
@@ -94,25 +104,44 @@ handleCornerCases p0@(Point (x0, y0)) p1@(Point (x1, y1)) fallover =
   where
     zero = Point (0, 0)
 
+inverseGuarded :: F -> F
+inverseGuarded n = cond ((n `eq` 0) .|. (n `eq` _p)) 1 (inverse n)
+
 add :: Point -> Point -> Comp Point
 add p0@(Point (x0, y0)) p1@(Point (x1, y1)) =
   handleCornerCases p0 p1 $ do
-    slopeDbl <- (* (x0 * x0 * 3 + _a)) <$> inverse (y0 + y0)
-    slopeAdd <- (* (_p + y1 - y0)) <$> inverse (_p + x1 - x0)
+    -- We need to handle the case of possible zero input for `inverse` because
+    -- the else branch of condPointM is a `Comp`, which is eagerly evaluated
+    -- regardless of the truth value of the predicate (during `intrepret` only?)
+    let slopeDbl = (x0 * x0 * 3 + _a) * inverseGuarded (y0 + y0)
+        slopeAdd = (_p + y1 - y0) * inverseGuarded (_p + x1 - x0)
     slope <- modP $ cond (p0 `eq` p1) slopeDbl slopeAdd
     x2 <- modP $ slope * slope + 2 * _p - (x0 + x1)
     y2 <- modP $ (x0 + _p - x2) * slope - y0
     return (Point (x2, y2))
 
-add' :: Comp (F, F)
-add' = do
-  x <- input Public
-  y <- input Public
-  Point (x', y') <- add (Point (x, y)) (Point (x, y))
-  return (x', y')
+smultVar :: F -> Point -> Comp (F, F)
+smultVar n p = do
+  assert =<< onCurve p
+  Point p' <- p `times` n
+  return p'
+  where
+    times :: Point -> F -> Comp Point
+    times point@(Point (_, _)) s = do
+      let bitsrev = reverse $ map (s !!!) [0 .. widthOf s - 1]
+      let f pt bit = do
+            p2 <- reuse =<< add pt pt
+            reuse =<< condPointM (complement bit) p2 (add p2 point)
+      foldM f (Point (0, 0)) bitsrev
 
 testScalarMult0 :: Comp ()
 testScalarMult0 = do
   (x', y') <- smult 123456 (Point (1341, 854))
+  assert $ x' `eq` 2560
+  assert $ y' `eq` 380
+
+testScalarMult1 :: Comp ()
+testScalarMult1 = do
+  (x', y') <- smultVar 123456 (Point (1341, 854))
   assert $ x' `eq` 2560
   assert $ y' `eq` 380
