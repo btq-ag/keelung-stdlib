9a1bda874051bfd2a8e04bfa062e7489dc8f3ef0
Ting-gian LUA <banacorn@gmail.com>
2023-09-07 17:02:37 +0800

    [ stash ]

--- src/Cipher/AES.hs
+++ src/Cipher/AES.hs
@@ -1,1 +1,19 @@
 module Cipher.AES where 
+
+import Keelung
+
+
+
+-- references
+-- https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.197.pdf
+-- https://opensource.apple.com/source/CommonCrypto/CommonCrypto-55010/Source/AESedp/AES.c.auto.html
+
+-- | Constants
+
+-- | Number of columns (32-bit words) comprising the State. For this standard, Nb = 4.
+nb :: Int
+nb = 4
+
+-- | KeyExpansion – round keys are derived from the cipher key using the AES key schedule. 
+--   AES requires a separate 128-bit round key block for each round plus one more.
+expandKey = undefined
