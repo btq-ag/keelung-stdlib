9edef6a354af84ce168b57240d808c7217590b1f
Ting-gian LUA <banacorn@gmail.com>
2023-10-04 20:42:19 +0800

    [ change ] Bump Keelung to v0.17.0

--- test/Test.hs
+++ test/Test.hs
@@ -1,9 +1,9 @@
 import qualified Test.Hash.Poseidon as Poseidon
-import qualified Test.Lib.Array as Array
-import qualified Test.Lib.W32 as W32
-import qualified Test.BLAKE2s as BLAKE2s
-import qualified Test.Util as Util
-import qualified Test.SHA256 as SHA256
+-- import qualified Test.Lib.Array as Array
+-- import qualified Test.Lib.W32 as W32
+-- import qualified Test.BLAKE2s as BLAKE2s
+-- import qualified Test.Util as Util
+-- import qualified Test.SHA256 as SHA256
 import Test.Tasty
 
 main :: IO ()
