e231d42e8020fc2aa4ecba2ee6e681071ba7dac0
Ting-gian LUA <banacorn@gmail.com>
2024-01-29 16:22:35 +0800

    [ new ] SBox built by tabulation

--- src/Cipher/AES.hs
+++ src/Cipher/AES.hs
@@ -1,4 +1,7 @@
 {-# LANGUAGE DataKinds #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use head" #-}
 
 module Cipher.AES where
 
@@ -27,41 +30,6 @@ import Keelung
 --       y2 <- sBox x2
 --       y3 <- sBox x3
 --       return (y0, y1, y2, y3)
-
--- -- | SBox
--- sBox :: Field -> Comp Field
--- sBox b = do
---   -- inverse of `b`
---   --    if b == 0
---   --        then 0
---   --        else b ^ (-1)
---   let inverse = cond (b `eq` 0) 0 (pow b 254)
---   -- convert the inverse to Byte so that we can manipulate the bits
---   b' <- toUInt 8 inverse :: Comp Byte
-
---   let b0 = b' !!! 0 .^. b' !!! 4 .^. b' !!! 5 .^. b' !!! 6 .^. b' !!! 7 .^. true
---   let b1 = b' !!! 1 .^. b' !!! 5 .^. b' !!! 6 .^. b' !!! 7 .^. b' !!! 0 .^. true
---   let b2 = b' !!! 2 .^. b' !!! 6 .^. b' !!! 7 .^. b' !!! 0 .^. b' !!! 1 .^. false
---   let b3 = b' !!! 3 .^. b' !!! 7 .^. b' !!! 0 .^. b' !!! 1 .^. b' !!! 2 .^. false
---   let b4 = b' !!! 4 .^. b' !!! 0 .^. b' !!! 1 .^. b' !!! 2 .^. b' !!! 3 .^. false
---   let b5 = b' !!! 5 .^. b' !!! 1 .^. b' !!! 2 .^. b' !!! 3 .^. b' !!! 4 .^. true
---   let b6 = b' !!! 6 .^. b' !!! 2 .^. b' !!! 3 .^. b' !!! 4 .^. b' !!! 5 .^. true
---   let b7 = b' !!! 7 .^. b' !!! 3 .^. b' !!! 4 .^. b' !!! 5 .^. b' !!! 6 .^. false
-
---   let result =
---         set (0, b0) $
---           set (1, b1) $
---             set (2, b2) $
---               set (3, b3) $
---                 set (4, b4) $
---                   set (5, b5) $
---                     set (6, b6) $
---                       set (7, b7) 0
-
---   toField result
---   where
---     set :: (Int, Boolean) -> Byte -> Byte
---     set (index, value) byte = setBit byte index value
 
 -- shiftRows :: (WordBlock, WordBlock, WordBlock, WordBlock) -> (WordBlock, WordBlock, WordBlock, WordBlock)
 -- shiftRows (r0, r1, r2, r3) = (r0, shiftWordBlock1 r1, shiftWordBlock2 r2, shiftWordBlock3 r3)
@@ -101,19 +69,99 @@ import Keelung
 --   field <- toField modulo
 --   return $ field + 99
 
+-- -- | SBox
+-- sBox :: Field -> Comp Field
+-- sBox b = do
+--   -- inverse of `b`
+--   --    if b == 0
+--   --        then 0
+--   --        else b ^ (-1)
+--   let inverse = cond (b `eq` 0) 0 (pow b 254)
+--   -- convert the inverse to Byte so that we can manipulate the bits
+--   b' <- toUInt 8 inverse :: Comp Byte
+
+--   let b0 = b' !!! 0 .^. b' !!! 4 .^. b' !!! 5 .^. b' !!! 6 .^. b' !!! 7 .^. true
+--   let b1 = b' !!! 1 .^. b' !!! 5 .^. b' !!! 6 .^. b' !!! 7 .^. b' !!! 0 .^. true
+--   let b2 = b' !!! 2 .^. b' !!! 6 .^. b' !!! 7 .^. b' !!! 0 .^. b' !!! 1 .^. false
+--   let b3 = b' !!! 3 .^. b' !!! 7 .^. b' !!! 0 .^. b' !!! 1 .^. b' !!! 2 .^. false
+--   let b4 = b' !!! 4 .^. b' !!! 0 .^. b' !!! 1 .^. b' !!! 2 .^. b' !!! 3 .^. false
+--   let b5 = b' !!! 5 .^. b' !!! 1 .^. b' !!! 2 .^. b' !!! 3 .^. b' !!! 4 .^. true
+--   let b6 = b' !!! 6 .^. b' !!! 2 .^. b' !!! 3 .^. b' !!! 4 .^. b' !!! 5 .^. true
+--   let b7 = b' !!! 7 .^. b' !!! 3 .^. b' !!! 4 .^. b' !!! 5 .^. b' !!! 6 .^. false
+
+--   let result =
+--         set (0, b0) $
+--           set (1, b1) $
+--             set (2, b2) $
+--               set (3, b3) $
+--                 set (4, b4) $
+--                   set (5, b5) $
+--                     set (6, b6) $
+--                       set (7, b7) 0
+
+--   toField result
+--   where
+--     set :: (Int, Boolean) -> Byte -> Byte
+--     set (index, value) byte = setBit byte index value
+
 -- | SBox but implemented in a more "analytical" way
 --      (b * 31 mod 257) + 99
-sBox3 :: Byte -> Comp Byte
-sBox3 b = do
-  -- inverse of `b`
-  --    if b == 0
-  --        then 0
-  --        else b ^ (-1)
-  -- let inverse = cond (b `eq` 0) 0 (pow b 254)
-  inverse <- freshVarUInt
-  assert ((inverse * b) `eq` 1)
-  (_, modulo) <- performDivMod inverse 257
-  return $ modulo + 99
+-- sBox3 :: Byte -> Comp Byte
+-- sBox3 b = do
+--   -- higher 4 bits of b
+--   let x = b !!! 4 .^. b !!! 5 .^. b !!! 6 .^. b !!! 7
+
+--   (_, modulo) <- performDivMod (b * 31) 257
+--   return $ modulo + 99
+
+-- | Choose from the given list of 16 bytes based on the lower 4 bits of the given byte
+genSBoxRow :: [Byte] -> Byte -> Byte
+genSBoxRow c x =
+  cond
+    (x !!! 3)
+    ( cond
+        (x !!! 2)
+        (cond (x !!! 1) (cond (x !!! 0) (c !! 15) (c !! 14)) (cond (x !!! 0) (c !! 13) (c !! 12))) -- 0b11__
+        (cond (x !!! 1) (cond (x !!! 0) (c !! 11) (c !! 10)) (cond (x !!! 0) (c !! 9) (c !! 8))) -- 0b10__
+    )
+    ( cond
+        (x !!! 2)
+        (cond (x !!! 1) (cond (x !!! 0) (c !! 7) (c !! 6)) (cond (x !!! 0) (c !! 5) (c !! 4))) -- 0b01__
+        (cond (x !!! 1) (cond (x !!! 0) (c !! 3) (c !! 2)) (cond (x !!! 0) (c !! 1) (c !! 0))) -- 0b00__
+    )
+
+-- | SBox, using Table 4 of the AES specification (page 14)
+-- | Choose from the given list of 16 16-bytes list based on the higher 4 bits of the given byte
+sBox :: Byte -> Byte
+sBox x =
+  cond
+    (x !!! 7)
+    ( cond
+        (x !!! 6)
+        ( cond
+            (x !!! 5)
+            (cond (x !!! 4) (genSBoxRow (sBoxTable !! 15) x) (genSBoxRow (sBoxTable !! 14) x)) -- 0b111_
+            (cond (x !!! 4) (genSBoxRow (sBoxTable !! 13) x) (genSBoxRow (sBoxTable !! 12) x)) -- 0b110_
+        )
+        ( cond
+            (x !!! 5)
+            (cond (x !!! 4) (genSBoxRow (sBoxTable !! 11) x) (genSBoxRow (sBoxTable !! 10) x)) -- 0b101_
+            (cond (x !!! 4) (genSBoxRow (sBoxTable !! 9) x) (genSBoxRow (sBoxTable !! 8) x)) -- 0b100_
+        )
+    )
+    ( cond
+        (x !!! 6)
+        ( cond
+            (x !!! 5)
+            (cond (x !!! 4) (genSBoxRow (sBoxTable !! 7) x) (genSBoxRow (sBoxTable !! 6) x)) -- 0b011_
+            (cond (x !!! 4) (genSBoxRow (sBoxTable !! 5) x) (genSBoxRow (sBoxTable !! 4) x)) -- 0b010_
+        )
+        ( cond
+            (x !!! 5)
+            (cond (x !!! 4) (genSBoxRow (sBoxTable !! 3) x) (genSBoxRow (sBoxTable !! 2) x)) -- 0b001_
+            (cond (x !!! 4) (genSBoxRow (sBoxTable !! 1) x) (genSBoxRow (sBoxTable !! 0) x)) -- 0b000_
+        )
+    )
 
 -- addRoundKey :: WordBlock -> WordBlock -> WordBlock
 -- addRoundKey (a0, a1, a2, a3) (b0, b1, b2, b3) = (a0 ^ b0, a1 ^ b1, a2 ^ b2, a3 ^ b3)
@@ -197,10 +245,10 @@ updateUIntWord w (c0, c1, c2, c3) i = do
 -- | SUBWORD
 subUIntWord :: UIntWord -> Comp UIntWord
 subUIntWord (a0, a1, a2, a3) = do
-  b0 <- sBox3 a0
-  b1 <- sBox3 a1
-  b2 <- sBox3 a2
-  b3 <- sBox3 a3
+  let b0 = sBox a0
+  let b1 = sBox a1
+  let b2 = sBox a2
+  let b3 = sBox a3
   return (b0, b1, b2, b3)
 
 -- | ROTWORD
